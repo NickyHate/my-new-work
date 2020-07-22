deed1bfe48b0916a18591be8badd4daacbc4a577
Никита <nikitostokarev22@gmail.com>
2020-07-22 19:05:30 +0300

    Updates

---
+++ precache-manifest.83ab8b4a70ef3e75ef4c031a525ab45f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703f2fc08233a8f2a33e693d6533c041",
+    "url": "/my-new-work/index.html"
+  },
+  {
+    "revision": "1a8c944b81ec7e26b566",
+    "url": "/my-new-work/static/css/main.9cd9d8b4.chunk.css"
+  },
+  {
+    "revision": "bc5618b2141cc58a80f2",
+    "url": "/my-new-work/static/js/2.5a84e8cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-new-work/static/js/2.5a84e8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a8c944b81ec7e26b566",
+    "url": "/my-new-work/static/js/main.4cf83823.chunk.js"
+  },
+  {
+    "revision": "7803b767a305bd3e285f",
+    "url": "/my-new-work/static/js/runtime-main.1cb8c130.js"
+  }
+]);
