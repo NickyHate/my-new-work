fe2b5553a18af1221ac55074b6ed7bbbf45c53cb
Никита <nikitostokarev22@gmail.com>
2020-07-23 21:54:01 +0300

    fix bug with input

--- src/components/input.js
+++ src/components/input.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Input({ value }) {
-  return <Input className="text-form" placeholder={value} />;
+  return <input className="text-form" placeholder={value} />;
 }
